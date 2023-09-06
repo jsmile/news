14d5415fe24818bbd8629fd10454a8e93af6e936
Byeong Gui Jeong <jsmile@naver.com>
2023-09-06 17:58:58 +0900

    ScanStreamTransformer

--- lib/src/blocs/stories_bloc/stories_bloc.dart
+++ lib/src/blocs/stories_bloc/stories_bloc.dart
@@ -4,6 +4,9 @@ import './stories_provider.dart';
 import '../../models/item_model.dart';
 import '../../resources/repository.dart';
 
+///
+/// Bloc - data 의 변화를 stream 에 반영하는 역할
+///
 class StoriesBloc {
   // data를 구할 수 있는 Repository 선언
   final _repository = Repository();
@@ -19,6 +22,21 @@ class StoriesBloc {
     final ids = await _repository.fetchTopIds();
     // 변화된 data( 값 )을 sink.add 를 통해 stream 에 추가함.
     _topIdsSubject.sink.add(ids);
+  }
+
+  /// stream 의 event 를 map 으로 변환하여 반환
+  /// - 복수개의 StreamBuidler 사용에 대응
+  _itemsTransformer() {
+    return ScanStreamTransformer(
+      // stream event 가 발생할 때마다 실행되는 함수
+      (Map<int, Future<ItemModel?>> cache, int id, index) {
+        // cache: map, id: 매 event 마다 전달되는 data
+        cache[id] = _repository.fetchItem(id); // map 에 data 추가
+        return cache;
+      },
+      // 이 함수가 사용하는 초기값( empty map )
+      <int, Future<ItemModel>>{},
+    );
   }
 
   dispose() {
